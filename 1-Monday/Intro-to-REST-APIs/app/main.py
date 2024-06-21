5f8835ce4210eedc0b6b194a0ba0907488152622
Your Name Comes Here <you@yourdomain.example.com>
2024-06-21 14:47:08 -0400

    added functionality in part 1 to import function from utils

--- 1-Monday/Intro-to-REST-APIs/app/main.py
+++ 1-Monday/Intro-to-REST-APIs/app/main.py
@@ -1,13 +1,21 @@
 from flask import Flask, jsonify
 from .api.example_api import example_bp  # Adjusted the import
 
+# from .utils.data import get_sample_data
+
 app = Flask(__name__)
 app.register_blueprint(example_bp, url_prefix="/api")
 
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Welcome to the Intro to REST APIs workshop!"})
+    # Combine the welcome message and sample data into one JSON response
+    return jsonify(
+        {
+            "message": "Welcome to the Intro to REST APIs workshop!",
+            # "sample_data": get_sample_data(),
+        }
+    )
 
 
 # Ignore favicon.ico requests
